eee9ec946290f8d840be19e9a1fb0870ab07a826
Piyush  Sanjay Mali <piyushmali2812@gmail.com>
2025-07-07 11:27:54 +0530

    Delete src directory

--- src/main/java/com/project/springbootapp/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.springbootapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.springbootapp.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-    Product findByName(String name);
-}
